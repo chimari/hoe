a5731dbba008f37b4c3a980de0c0983407fd3201
Akito Tajitsu <tajitsu@naoj.org>
2019-03-29 20:59:33 -1000

    modified markup labels

--- main.h
+++ main.h
@@ -58,7 +58,8 @@
 #endif
 
 // local headers
-#include"configfile.h"
+#include "configfile.h"
+#include "gtkut.h"
 
 #include "libnova/libnova.h"
 #include "resources.h"
@@ -2965,25 +2966,6 @@ void my_signal_connect();
 void my_entry_set_width_chars();
 gchar* check_ext();
 gchar* make_head();
-void gtkut_tree_view_column_set_markup();
-GtkWidget * gtkut_label_new();
-GtkWidget * gtkut_frame_new();
-void gtkut_frame_set_label();
-GtkWidget * gtkut_hbox_new();
-GtkWidget * gtkut_vbox_new();
-GtkWidget * gtkut_table_new();
-void gtkut_table_attach();
-void gtkut_table_attach_defaults();
-#ifdef USE_GTK3
-GtkWidget * gtkut_button_new_from_icon_name();
-GtkWidget * gtkut_toggle_button_new_from_icon_name();
-GtkWidget * gtkut_image_menu_item_new_with_label();;
-#else
-GtkWidget * gtkut_button_new_from_stock();
-GtkWidget * gtkut_toggle_button_new_from_stock();
-#endif
-GtkWidget * gtkut_button_new_from_pixbuf();
-GtkWidget * gtkut_toggle_button_new_from_pixbuf();
 #ifdef USE_WIN32
 gchar* WindowsVersion();
 #endif
